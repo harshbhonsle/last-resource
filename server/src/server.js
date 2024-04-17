e6dd08ecc22f13a50620fdf5617b97957540eddc
Harsh <bhonsleharsh16@gmail.com>
2024-04-17 22:00:12 +0530

    Add routes for health and db creation

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import 'dotenv/config';
+import pool from './db/pool.js';
 
 const app = express();
+
+app.get('/db', async (req, res) => {
+	try {
+		await pool.query(`CREATE TABLE IF NOT EXISTS computer_network (
+			id SERIAL PRIMARY KEY,
+			file_name VARCHAR(255) UNIQUE NOT NULL,
+			file_data BYTEA
+		)`)
+
+		res.json({ message: '\'coputer_network\' table created.' });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error });
+	}
+});
+
+app.get('/health', (req, res) => {
+	res.json({ message : 'OK' });
+})
 
 app.listen(3000, () => {
 	console.log("Server listening on port 3000");
