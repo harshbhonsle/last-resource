244812f70daaaa2bd4412501cda556cfe64a8fc9
Harsh <bhonsleharsh16@gmail.com>
2024-04-20 21:09:24 +0530

    Fetch subject files on page mount

--- client/src/components/Accordion.jsx
+++ client/src/components/Accordion.jsx
@@ -1,16 +1,18 @@
 
-const Accordion=({ examName, files })=>{
+const Accordion=({ examName, links })=>{
     return(
-        <>
-        <div className="border border-green-600 ">
-
-        <h1 className=" py-3 bg-[#F38181] hover:bg-red-400 active:selection  border border-blue-500 text-center font-semibold">
-            {examName}
-        </h1>
-        <h1>Hello</h1>
+        <div>
+            <h1 className=" py-3 bg-[#F38181] hover:bg-red-400 text-center font-semibold mb-3">
+                {examName}
+            </h1>
+            <div className="grid grid-cols-3 gap-4">
+                {links && links.map((link, index) => (
+                    <button key={index} className="px-4 py-1 border border-green-500 rounded-lg bg-gray-100 hover:border-[#FCE38A]">
+                        <a target="_blank" href={link.url}>{link.name}</a>
+                    </button>
+                ))}
+            </div>
         </div>
-
-        </>
     )
 }
 export default Accordion;
--- client/src/pages/Subject.jsx
+++ client/src/pages/Subject.jsx
@@ -1,5 +1,7 @@
 import {useParams} from 'react-router-dom'
 import Accordion from '../components/Accordion';
+import { useEffect, useState } from 'react';
+
 const capitalFirstWord = (word) => {
     // let newWord = '';
 
@@ -14,19 +16,57 @@ const capitalFirstWord = (word) => {
     return word[0].toUpperCase() + word.substr(1, word.length);
 }
 
+
+
 const Subject =()=>{
     const {subjectID} = useParams();
-
     const subjectName = subjectID.split('-').map(word => capitalFirstWord(word)).join(' ');
+
+    const [error, setError] = useState('');
+    const [subjectFileLinks, setSubjectFileLinks] = useState([]);
+
+    useEffect(() => {
+        const fetchFiles = async () => {
+            try {
+                const res = await fetch(`http://localhost:3000/subjects/${subjectID}`);
+                const data = await res.json();
+
+                if (res.status === 404) {
+                    setError('Page does not exist');
+                }
+
+                if (res.ok) {
+                    setSubjectFileLinks(data);
+                    setError('');
+                }
+
+                if (!res.ok) {
+                    setError('Some error occured');
+                    setSubjectFileLinks([]);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchFiles();
+    }, [])
+
     return(
         <>
          <div className="relative h-screen pt-24">
             <h1 className='font-bold mt-12 text-center text-5xl underline'>{subjectName}</h1>
-            <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/8 w-1/2 flex flex-col gap-3'>
-                <Accordion examName='FA' />
-                <Accordion examName='SA' />
-                <Accordion examName='ETE' />
-            </div>
+            {(!error && subjectFileLinks) ? (
+                <div className='absolute left-1/2 -translate-x-1/2 mt-20 w-1/2 flex flex-col gap-12'>
+                    <Accordion examName='FA' links={subjectFileLinks.fa} />
+                    <Accordion examName='SA' links={subjectFileLinks.st} />
+                    <Accordion examName='ETE' links={subjectFileLinks.ete} />
+                </div>
+            ) : (
+                <div className='absolute left-1/2 w-fit -translate-x-1/2 -translate-y-1/2 mt-20 w-1/2 flex flex-col gap-12 text-4xl font-bold'>
+                    Bhai Sorry :(
+                </div>
+            )}
          </div>
          
         </>
